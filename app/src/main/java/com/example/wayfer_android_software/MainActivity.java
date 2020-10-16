f3070ac64c83d729e37cacffcb640f9a99f9b0cd
TRwater <gk554466gk@gmail.com>
2020-10-16 21:24:04 +0900

    nn

--- app/src/main/java/com/example/wayfer_android_software/MainActivity.java
+++ app/src/main/java/com/example/wayfer_android_software/MainActivity.java
@@ -1,14 +1,81 @@
 package com.example.wayfer_android_software;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.CompoundButton;
+        import android.widget.Switch;
+        import android.widget.Toast;
 
-import android.os.Bundle;
+        import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button1;
+    private Switch switch1;
+    private Switch switch2;
+    private Switch switch3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switch1=(Switch) findViewById(R.id.switch1);
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked){
+                    Toast.makeText(MainActivity.this, "wayfer와 연결이 실행되었습니다. ", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "wayfer와 연결이 해제되었습니다. ", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+        switch2=(Switch) findViewById(R.id.switch2);
+        switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked){
+                    Toast.makeText(MainActivity.this, "TTS/음성모드가 실행되었습니다. ", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "TTS/음성모드가 해제되었습니다. ", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+        switch3=(Switch) findViewById(R.id.switch3);
+        switch3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked){
+                    Toast.makeText(MainActivity.this, "알림모드가 실행되었습니다. ", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "알림모드가 해제되었습니다. ", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+
+        button1=findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,HelpActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
