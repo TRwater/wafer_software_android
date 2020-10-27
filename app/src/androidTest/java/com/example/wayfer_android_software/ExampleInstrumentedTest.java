db928283b6718a560b3b533c4d8e2f75115e341c
TRwater <gk554466gk@gmail.com>
2020-10-27 18:15:33 +0900

    new

--- app/src/androidTest/java/com/example/wayfer_android_software/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/wayfer_android_software/ExampleInstrumentedTest.java
@@ -1,26 +1,81 @@
 package com.example.wayfer_android_software;
 
-import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.appcompat.app.AppCompatActivity;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+class ExampleinstrmentedTest extends AppCompatActivity {
 
-import static org.junit.Assert.*;
+    private Button button1;
+    private Switch switch1;
+    private Switch switch2;
+    private Switch switch3;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.wayfer_android_software", appContext.getPackageName());
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        switch1=(Switch) findViewById(R.id.switch1);
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked){
+                    Toast.makeText(ExampleinstrmentedTest.this, "wayfer와 연결이 실행되었습니다. ", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(ExampleinstrmentedTest.this, "wayfer와 연결이 해제되었습니다. ", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+        switch2=(Switch) findViewById(R.id.switch2);
+        switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked){
+                    Toast.makeText(ExampleinstrmentedTest.this, "TTS/음성모드가 실행되었습니다. ", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(ExampleinstrmentedTest.this, "TTS/음성모드가 해제되었습니다. ", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+        switch3=(Switch) findViewById(R.id.switch3);
+        switch3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked){
+                    Toast.makeText(ExampleinstrmentedTest.this, "알림모드가 실행되었습니다. ", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(ExampleinstrmentedTest.this, "알림모드가 해제되었습니다. ", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+
+        button1=findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(ExampleinstrmentedTest.this,HelpActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/wayfer_android_software/MainActivity.java
+++ app/src/main/java/com/example/wayfer_android_software/MainActivity.java
@@ -6,39 +6,101 @@ package com.example.wayfer_android_software;
         import android.widget.Button;
         import android.widget.CompoundButton;
         import android.widget.Switch;
+        import android.widget.TextView;
         import android.widget.Toast;
+        import android.bluetooth.BluetoothAdapter;
+        import android.bluetooth.BluetoothDevice;
+        import android.bluetooth.BluetoothSocket;
+        import android.content.DialogInterface;
+        import android.os.Handler;
+        import android.os.SystemClock;
 
+        import androidx.appcompat.app.AlertDialog;
         import androidx.appcompat.app.AppCompatActivity;
+
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.OutputStream;
+        import java.io.UnsupportedEncodingException;
+        import java.util.ArrayList;
+        import java.util.List;
+        import java.util.Set;
+        import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button1;
-    private Switch switch1;
-    private Switch switch2;
-    private Switch switch3;
+    TextView mTvBluetoothStatus;
+    Button button1;
+    Switch switch1;
+    Switch switch2;
+    Switch switch3;
+    Switch switch4;
+
+    BluetoothAdapter mBluetoothAdapter;
+    Set<BluetoothDevice> mPairedDevices;
+    List<String> mListPairedDevices;
+
+    Handler mBluetoothHandler;
+    ConnectedBluetoothThread mThreadConnectedBluetooth;
+    BluetoothDevice mBluetoothDevice;
+    BluetoothSocket mBluetoothSocket;
+
+    final static int BT_REQUEST_ENABLE = 1;
+    final static int BT_MESSAGE_READ = 2;
+    final static int BT_CONNECTING_STATUS = 3;
+    final static UUID BT_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        mTvBluetoothStatus = (TextView)findViewById(R.id.TvBluetoothStatus);
         switch1=(Switch) findViewById(R.id.switch1);
+        switch2=(Switch) findViewById(R.id.switch2);
+        switch3=(Switch) findViewById(R.id.switch3);
+        switch4=(Switch) findViewById(R.id.switch4);
+        button1=findViewById(R.id.button1);
+
+
         switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
                 if (isChecked){
+                    bluetoothOn();
                     Toast.makeText(MainActivity.this, "wayfer와 연결이 실행되었습니다. ", Toast.LENGTH_SHORT).show();
                 }
                 else{
+                    bluetoothOff();
                     Toast.makeText(MainActivity.this, "wayfer와 연결이 해제되었습니다. ", Toast.LENGTH_SHORT).show();
                 }
             }
 
         });
-        switch2=(Switch) findViewById(R.id.switch2);
+
         switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked){
+                    listPairedDevices();
+                    Toast.makeText(MainActivity.this, "wayfer와 연결이 실행되었습니다. ", Toast.LENGTH_SHORT).show();
+                }
+                else{
+
+                    Toast.makeText(MainActivity.this, "wayfer와 연결이 해제되었습니다. ", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+
+
+        switch3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -52,8 +114,8 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-        switch3=(Switch) findViewById(R.id.switch3);
-        switch3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+        switch4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -68,7 +130,6 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-        button1=findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -77,5 +138,152 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    void bluetoothOn() {
+        if(mBluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(), "블루투스를 지원하지 않는 기기입니다.", Toast.LENGTH_LONG).show();
+        }
+        else {
+            if (mBluetoothAdapter.isEnabled()) {
+                Toast.makeText(getApplicationContext(), "블루투스가 이미 활성화 되어 있습니다.", Toast.LENGTH_LONG).show();
+                mTvBluetoothStatus.setText("활성화");
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "블루투스가 활성화 되어 있지 않습니다.", Toast.LENGTH_LONG).show();
+                Intent intentBluetoothEnable = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(intentBluetoothEnable, BT_REQUEST_ENABLE);
+            }
+        }
+    }
+    void bluetoothOff() {
+        if (mBluetoothAdapter.isEnabled()) {
+            mBluetoothAdapter.disable();
+            Toast.makeText(getApplicationContext(), "블루투스가 비활성화 되었습니다.", Toast.LENGTH_SHORT).show();
+            mTvBluetoothStatus.setText("비활성화");
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "블루투스가 이미 비활성화 되어 있습니다.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case BT_REQUEST_ENABLE:
+                if (resultCode == RESULT_OK) { // 블루투스 활성화를 확인을 클릭하였다면
+                    Toast.makeText(getApplicationContext(), "블루투스 활성화", Toast.LENGTH_LONG).show();
+                    mTvBluetoothStatus.setText("블루투스 연결상태: 활성화");
+                } else if (resultCode == RESULT_CANCELED) { // 블루투스 활성화를 취소를 클릭하였다면
+                    Toast.makeText(getApplicationContext(), "취소", Toast.LENGTH_LONG).show();
+                    mTvBluetoothStatus.setText("불루투스 연결상태: 비활성화");
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    void listPairedDevices() {
+        if (mBluetoothAdapter.isEnabled()) {
+            mPairedDevices = mBluetoothAdapter.getBondedDevices();
+
+            if (mPairedDevices.size() > 0) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("장치 선택");
+
+                mListPairedDevices = new ArrayList<String>();
+                for (BluetoothDevice device : mPairedDevices) {
+                    mListPairedDevices.add(device.getName());
+                    //mListPairedDevices.add(device.getName() + "\n" + device.getAddress());
+                }
+                final CharSequence[] items = mListPairedDevices.toArray(new CharSequence[mListPairedDevices.size()]);
+                mListPairedDevices.toArray(new CharSequence[mListPairedDevices.size()]);
+
+                builder.setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int item) {
+                        connectSelectedDevice(items[item].toString());
+                    }
+                });
+                AlertDialog alert = builder.create();
+                alert.show();
+            } else {
+                Toast.makeText(getApplicationContext(), "페어링된 장치가 없습니다.", Toast.LENGTH_LONG).show();
+            }
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "블루투스가 비활성화 되어 있습니다.", Toast.LENGTH_SHORT).show();
+        }
+    }
+    void connectSelectedDevice(String selectedDeviceName) {
+        for(BluetoothDevice tempDevice : mPairedDevices) {
+            if (selectedDeviceName.equals(tempDevice.getName())) {
+                mBluetoothDevice = tempDevice;
+                break;
+            }
+        }
+        try {
+            mBluetoothSocket = mBluetoothDevice.createRfcommSocketToServiceRecord(BT_UUID);
+            mBluetoothSocket.connect();
+            mThreadConnectedBluetooth = new ConnectedBluetoothThread(mBluetoothSocket);
+            mThreadConnectedBluetooth.start();
+            mBluetoothHandler.obtainMessage(BT_CONNECTING_STATUS, 1, -1).sendToTarget();
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(), "블루투스 연결 중 오류가 발생했습니다.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private class ConnectedBluetoothThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedBluetoothThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(), "소켓 연결 중 오류가 발생했습니다.", Toast.LENGTH_LONG).show();
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+        public void run() {
+            byte[] buffer = new byte[1024];
+            int bytes;
+
+            while (true) {
+                try {
+                    bytes = mmInStream.available();
+                    if (bytes != 0) {
+                        SystemClock.sleep(100);
+                        bytes = mmInStream.available();
+                        bytes = mmInStream.read(buffer, 0, bytes);
+                        mBluetoothHandler.obtainMessage(BT_MESSAGE_READ, bytes, -1, buffer).sendToTarget();
+                    }
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+        public void write(String str) {
+            byte[] bytes = str.getBytes();
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(), "데이터 전송 중 오류가 발생했습니다.", Toast.LENGTH_LONG).show();
+            }
+        }
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(), "소켓 해제 중 오류가 발생했습니다.", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
